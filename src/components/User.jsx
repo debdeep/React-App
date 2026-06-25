23b08b5555cb0b39adb4541d328e64ff56709f5d
debdeep <debdeepm89@gmail.com>
2026-06-25 22:26:39 +0530

    git api integration for about us section data sync

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,22 @@
-const User = () => {
+const User = ({ userData }) => {
+    console.log("userData:", userData);
+    const { name, bio, html_url, location, email, company, followers, public_repos } = userData;
     return (
         <div className="user-card">
-            <h2>Name: Debdeep (function based)</h2>
-            <h3>Location: Pune</h3>
-            <h4>Contact: debdeepm89@gmail.com</h4>
+            <h2>Name: {name}</h2>
+            <span>{bio}</span>
+            <ul>
+                <li><a href={html_url} target="__blank">Open Git Profile</a></li>
+                <li>Location: {location? location: '-'}</li>
+                <li>Contact: {email? email: '-'} </li>
+                <li>Company: {company? company: 'NA'}</li>
+                <li>Followers: {followers? followers: 0}</li>
+                <li>No of Repos: {public_repos? public_repos: 0}</li>
+            </ul>
+
+            <span></span>
+            <span></span>
+            <span></span>
         </div>
     )
 }
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,11 +1,20 @@
 import User from "../components/User";
 import UserClass from "../components/UserClass";
+import { useState, useEffect } from "react";
 
 const AboutUs = () => {
+    const [userApiData, setUserApiData] = useState([]);
+    useEffect(() => {
+        getProfileData();
+    }, []);
+    const getProfileData = async () => {
+        const response = await ((await fetch("https://api.github.com/users/debdeep")).json());
+        setUserApiData(response);
+    }
     return (
         <>
             <h1>Welcome to About us Page</h1>
-            <User />
+            <User userData={userApiData} />
             {/* <UserClass name="Debdeep(class based)" location="Pune" contact="debdeepm89@gmail.com" /> */}
         </>
     );
