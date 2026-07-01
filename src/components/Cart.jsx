081ab58380284c9aa056f4fdbe24b82070c91829
debdeep <debdeepm89@gmail.com>
2026-07-01 16:31:07 +0530

    clear cart functionality added

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,18 +1,27 @@
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { clearCart } from "../utils/store/cartSlice";
 import CartItem from "./CartItem";
 
 const Cart = () => {
     const cartItems = useSelector((store) => store.cart.items);
-    const totalItems = cartItems.reduce((sum, item) => sum + (item?.quantity || 1), 0);
-    const totalAmount = cartItems.reduce((sum, item) => {
+    const dispatch = useDispatch();
+
+    const totalItems = cartItems?.reduce((sum, item) => sum + (item?.quantity || 1), 0);
+
+    const totalAmount = cartItems?.reduce((sum, item) => {
         const price = item?.price || item?.defaultPrice || 0;
         return sum + price * (item?.quantity || 1);
     }, 0);
 
+    const clearCartItems = () => {
+        dispatch(clearCart());
+    };
+
     return (
         <div className="cart">
             <h2>Your Cart</h2>
-
+            <button className="clear-cart" onClick={clearCartItems}>Clear Cart</button>
             {cartItems?.length === 0 ? (
                 <p>Your cart is empty.</p>
             ) : (
